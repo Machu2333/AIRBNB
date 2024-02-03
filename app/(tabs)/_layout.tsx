f47cd38c17a889285123fa62da3d7509e31a748e
liuziyuan <973830762@qq.com>
2024-02-03 23:58:32 +0800

    11

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,59 +1,52 @@
-import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Colors from '@/constants/Colors'
+import { FontAwesome, Ionicons, Entypo, FontAwesome5, MaterialCommunityIcons, AntDesign, Feather } from '@expo/vector-icons';
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const layout = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}>
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+          fontFamily: "Lobster",
+        }
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name='index'
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
+          tabBarLabel: "Explre",
+          tabBarIcon: ({ color, size }) => <Feather name="search" size={size} color={color} />
+        }} />
       <Tabs.Screen
-        name="two"
+        name='wishlists'
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
+          tabBarLabel: "Wishlists",
+          tabBarIcon: ({ color, size }) => <Entypo name="heart-outlined" size={size} color={color} />
+        }} />
+      <Tabs.Screen
+        name='trips'
+        options={{
+          tabBarLabel: "Trips",
+          tabBarIcon: ({ color, size }) => <FontAwesome5 name="airbnb" size={size} color={color} />
+        }} />
+      <Tabs.Screen
+        name='inbox'
+        options={{
+          tabBarLabel: "Inbox",
+          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="message-outline" size={size} color={color} />
+        }} />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => <Ionicons name="person-circle-outline" size={size} color={color} />
+        }} />
+
+
     </Tabs>
-  );
+  )
 }
+
+export default layout
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,35 @@
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, ThemeProvider, useRoute } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, router, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
 
-import { useColorScheme } from '@/components/useColorScheme';
+// Import your publishable key
+const CLERK_PUBLISHABLE_KEY = "pk_test_bWFqb3Itd2hhbGUtMzIuY2xlcmsuYWNjb3VudHMuZGV2JA"
+if (!CLERK_PUBLISHABLE_KEY) {
+  console.log("Missing Publishable Key")
+}
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key)
+    } catch (error) {
+      return null
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItem(key, value)
+    } catch (error) {
+      return
+    }
+  }
+}
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -23,7 +47,10 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
+    GreatVibes: require('../assets/fonts/GreatVibes-Regular.ttf'),
+    Lobster: require('../assets/fonts/Lobster/Lobster-Regular.ttf'),
+
+    // ...FontAwesome.font,
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -41,18 +68,62 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+
+  return <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={tokenCache}>
+    <RootLayoutNav />
+
+  </ClerkProvider>
+
+
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  const router = useRouter();
+  const { isLoaded, isSignedIn } = useAuth();
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.push("/(modals)/login")
+    }
+  }, [isLoaded])
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)"
+        options={{
+          headerShown: false,
+          headerTitleAlign: "center"
+        }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: "Log in or Sign ip",
+          headerTitleStyle: {
+            fontFamily: "Lobster",
+          },
+          headerTitleAlign: "center",
+          presentation: "modal",
+          // headerBackTitleVisible: true,
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => {
+              router.back()
+            }}>
+              <Ionicons name='close-outline' size={24}></Ionicons>
+            </TouchableOpacity>
+          )
+        }} />
+      <Stack.Screen
+        name="listing/[id]"
+        options={{
+          headerTitle: ""
+        }} />
+      <Stack.Screen
+        name="(modals)/booking"
+
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
+        }} />
+
+    </Stack>
   );
 }
